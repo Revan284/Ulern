fbe521b55694310f84ee3501c942d1cab9f797a5
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 15:27:31 +0800

    Add files via upload

---
+++ 1 неделя/Bitcoin to the masses!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bitcoin_to_the_masses_
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            double amount = 1.11; //количество биткоинов от одного человека
+            int peopleCount = 60; // количество человек
+
+            // int totalMoney = (int)amount * peopleCount; ← исправьте ошибку в этой строке
+
+            int totalMoney = (int)Math.Round(amount * peopleCount); 
+            Console.WriteLine(totalMoney);
+            Console.ReadKey();
+        }
+    }
+}
