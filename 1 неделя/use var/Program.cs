895e8135b5506caf690a2960a26daccbd7be5f8e
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 15:46:37 +0800

    Add files via upload

---
+++ 1 неделя/use var/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace use_var
+{
+    internal class Program
+    {
+        static public void Main()
+        {
+            // var a = 5;  ← исправьте эту строку
+            var a = 5.0;
+            a += 0.5;
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
