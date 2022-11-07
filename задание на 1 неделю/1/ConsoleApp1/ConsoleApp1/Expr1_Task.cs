7668febe2d3c02fcf417222ece285f1589f5547b
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 16:37:24 +0800

    Add files via upload

---
+++ задание на 1 неделю/1/ConsoleApp1/ConsoleApp1/Expr1_Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Expr1_Task
+    {
+        internal static void Exchange(int a, int b)
+        {
+            // ваш код
+            var surname = "Пупкин";
+            var name = "Иван";
+            Console.WriteLine($"Ваше Имя - {surname}. Ваша Фамилия - {name}");
+
+            (surname,name) = (name,surname);
+            Console.WriteLine($"Ваше Имя - {surname}. Ваша Фамилия - {name}");
+
+            if ((a == Program.y) & (b == Program.x))
+                Console.Write("{0}{1}", ((char)a), ((char)b));
+        }
+    }
+}
