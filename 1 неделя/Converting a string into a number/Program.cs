58ed722f49c49201690212a6cc781e1e296a6c86
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 15:33:11 +0800

    Add files via upload

---
+++ 1 неделя/Converting a string into a number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Converting_a_string_into_a_number
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string doubleNumber = "894376.243643";
+            //int number = doubleNumber; Вася уверен, что ошибка где-то тут
+            double number = double.Parse(doubleNumber, CultureInfo.InvariantCulture);
+            Console.WriteLine(number + 1);
+            Console.ReadKey();
+        }
+    }
+}
