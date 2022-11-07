efd51d875b2153b6771e61353ed606986232835a
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 15:23:15 +0800

    Add files via upload

---
+++ 1 неделя/Type conversion errors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Type_conversion_errors
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            //double pi = Math.PI;
+            //int tenThousand = 10000L;
+            //float tenThousandPi = pi * tenThousand;
+            //int roundedTenThousandPi = tenThousandPi;
+            //int integerPartOfTenThousandPi = tenThousandPi;
+
+            double pi = Math.PI;
+            long tenThousand = 10000L;
+            double tenThousandPi = pi * tenThousand;
+            int roundedTenThousandPi = (int)Math.Round(tenThousandPi);
+            int integerPartOfTenThousandPi = (int)tenThousandPi;
+
+            Console.WriteLine(integerPartOfTenThousandPi);
+            Console.WriteLine(roundedTenThousandPi);
+            Console.ReadKey();
+        }
+    }
+}
