ebd50c44e43a2fb46be10c48473d8445d6fbb451
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 16:53:44 +0800

    Add files via upload

---
+++ задание на 1 неделю/2/ConsoleApp1/ConsoleApp1/Expr2_Task.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    class Expr2_Task
+    {
+        /* Задается натуральное трехзначное число (гарантируется, 
+         * что трехзначное). Развернуть его, т.е. получить трехзначное число,
+         * записанное теми же цифрами в обратном порядке. */
+        internal static int Reverse(int a)
+        {
+            // ваш код
+            var userNumber = a;
+            var units = userNumber % 10;
+            var tens = (userNumber / 10) % 10;
+            var hundreads = userNumber / 100;
+
+            return units * 100 + tens * 10 + hundreads;
+            
+        }
+    }
+}
---
+++ задание на 1 неделю/2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool err = false;
+            var rand = new Random((int)DateTime.Now.Millisecond);
+            for (int i = 0; i < 1000; i++)
+            {
+                int a = rand.Next(100, 1000);
+                var actual = Expr2_Task.Reverse(a);
+                var expect = Reversion(a);
+                if (actual != expect)
+                {
+                    Console.WriteLine("Error For Number: {0}", a);
+                    Console.WriteLine("Actual : {0}", actual);
+                    Console.WriteLine("Expect : {0}", expect);
+                    err = true;
+                    break;
+                }
+                if (i % 100 == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Выполнено {0}%", i / 10);
+                }
+            }
+            if (err == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Все тесты пройдены. Задание выполнено!");
+            }
+            Console.ReadKey();
+        }
+        static int Reversion(int a) => int.Parse(string.Concat(a.ToString().Reverse().ToArray()));
+    }
+}
