af10834710937906612a7859a9ddd019e40dbfee
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 16:02:30 +0800

    Add files via upload

---
+++ 1 неделя/We’re looking for methods!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace We_re_looking_for_methods_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetLastHalf("I love CSharp!"));
+            Console.WriteLine(GetLastHalf("1234567890"));
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+            Console.ReadKey();
+        }
+        static string GetLastHalf(string text)
+        {
+            return text.Substring(text.Length / 2).Replace(" ", "");
+        }
+    }
+}
