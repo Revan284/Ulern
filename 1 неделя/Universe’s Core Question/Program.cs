0436ec39b9600f38fa1adb91ff62147c3ca03cc4
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 15:59:33 +0800

    Add files via upload

---
+++ 1 неделя/Universe’s Core Question/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Universe_s_Core_Question
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Print(GetSquare(42));
+            Console.ReadKey();
+        }
+
+        static double GetSquare(double userNumber)
+        {
+            return userNumber * userNumber;
+        }
+
+        static void Print(double sqareUserNumber)
+        {
+            Console.WriteLine(sqareUserNumber);
+        }
+    }
+}
