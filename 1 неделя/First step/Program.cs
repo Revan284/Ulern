444403582a9d2a39deb7335cbafe2c1d38cdbb60
Revan284 <107996707+Revan284@users.noreply.github.com>
2022-11-07 15:06:23 +0800

    Add files via upload

---
+++ 1 неделя/First step/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Первый_шаг
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("The first step!");
+            Console.ReadKey();
+        }
+    }
+}
